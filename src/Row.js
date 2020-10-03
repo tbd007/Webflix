a0a67ca95abb7e61009ef8a901b410061c675497
Tanja Babic-Debrosse <58664635+tbd007@users.noreply.github.com>
2020-10-03 14:46:21 +0200

    trailers

--- src/Row.js
+++ src/Row.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "./axios";
 import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original"; //base url for images src
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     //when row loads this will run once if [], if [movies] everytime movies changes
@@ -27,14 +29,27 @@ function Row({ title, fetchUrl, isLargeRow }) {
     },
   };
 
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
-
       <div className="row_posters">
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row_poster ${isLargeRow && "row_posterLarge"}`} //if Row isLargeRow in App.js it applies the row_posterLarge css rules
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path //applies different image types depending on isLargeRow
@@ -43,7 +58,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
-      <YouTube videoId={trailerUrl} opts={opts} />
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
